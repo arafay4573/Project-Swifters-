491638cb98aa465135fbfa671e2a15cb0d83a4aa
Abdur Rafay <rafay4573@gmail.com>
2025-11-09 20:01:09 +0500

    Add server listener to index.js

--- api/index.js
+++ api/index.js
@@ -29,4 +29,8 @@ app.get('/', (req, res) => {
   res.send('Welcome to Project Swifters API');
 });
 
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server running on port ${process.env.PORT || 5000}`);
+})
+
 module.exports = app;
